ac2a98f69ca72c959765feefbf4479a55aa2356a
Sourajeet Mohanty <47032030+sourajeetmohanty@users.noreply.github.com>
2020-05-25 11:44:53 +0530

    Create weird-algo.cpp

---
+++ weird-algo.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include<bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main()
+{    
+   long int n;
+   cin>>n;
+  while(true)
+  {
+  	cout<<n<<" ";
+  	if(n==1)
+  	break;
+  	if(n%2==0)
+  	{
+  		n/=2;
+	  }
+	  else
+	  {
+	  	n*=3;
+	  	n+=1;
+	  }
+	  
+  	
+  }
+    return 0;
+}
