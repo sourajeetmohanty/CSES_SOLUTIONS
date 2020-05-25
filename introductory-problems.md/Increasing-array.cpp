a6a8ed24d037228a9a9617219d7b796791c91938
Sourajeet Mohanty <47032030+sourajeetmohanty@users.noreply.github.com>
2020-05-25 11:51:19 +0530

    Create Increasing-array.cpp

---
+++ introductory-problems.md/Increasing-array.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include<bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main()
+{ long long int n,d=0;
+  cin>>n;
+  int a[n];
+  for(int i=0;i<n;i++)
+  {
+  	cin>>a[i];
+  	
+  }
+  for(int i=1;i<n;i++)
+  {  
+  	if(a[i]<a[i-1])
+   {
+   	d+=a[i-1]-a[i];
+   	a[i]=a[i-1];
+   }
+ 
+ 
+  }
+  cout<<d;
+  
+  
+  
+  
+}
